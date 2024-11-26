846aa8450b7dbcf77c742edbd0094dd50274a545
Ivan Berlin <contact@godmod.dev>
2024-11-26 23:12:38 +0300

    wip

--- internal/service/s3/upload.go
+++ internal/service/s3/upload.go
@@ -16,8 +16,8 @@ func (s *Service) UploadFile(ctx context.Context, file models.File, isPrivate bo
 
 	info, err := s.minio.PutObject(context.Background(), models.BucketName, objectName, file.Data, -1, minio.PutObjectOptions{
 		UserMetadata: map[string]string{
-			"User_id": strconv.FormatInt(int64(contextor.GetUserID(ctx)), 10),
-			"Private": strconv.FormatBool(isPrivate),
+			models.MetaUserID:     strconv.FormatInt(int64(contextor.GetUserID(ctx)), 10),
+			models.MetaPrivateKey: strconv.FormatBool(isPrivate),
 		},
 	})
 	if err != nil {
